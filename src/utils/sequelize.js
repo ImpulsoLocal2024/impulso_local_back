07ef87502217e2d8fa9d9a8ec6f7860b5434a64c
Jonathan Rodriguez <jonathan@Mac-mini-de-Jonathan.local>
2024-10-24 13:21:08 -0500

    2doCommit

--- src/utils/sequelize.js
+++ src/utils/sequelize.js
@@ -4,6 +4,12 @@ require('dotenv').config();
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // Esto es útil si el certificado SSL es autofirmado o no está verificado
+    },
+  },
 });
 
 // Verificar conexión
